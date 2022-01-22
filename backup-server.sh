19f644a21ec26fe84cf8a1bb8badeb8623ab1ad9
Marek Makovec <marek.makovec@spaceflow.io>
2022-01-22 12:17:57 +0100

    Initial version

---
+++ backup-server.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+source config.sh
+
+### Check if properly configured
+
+if [ -z ${REMOTE_SERVER_USER_NAME+x} ]; then echo "Variable 'REMOTE_SERVER_USER_NAME' is not defined! Terminating. "; exit 1; fi
+if [ -z ${REMOTE_SERVER_USER_PASSWORD+x} ]; then echo "Variable 'REMOTE_SERVER_USER_PASSWORD' is not defined! Terminating. "; exit 1; fi
+if [ -z ${REMOTE_SERVER_IP+x} ]; then echo "Variable 'REMOTE_SERVER_IP' is not defined! Terminating. "; exit 1; fi
+if [ -z ${REMOTE_SERVER_DIRECTORY+x} ]; then echo "Variable 'REMOTE_SERVER_DIRECTORY' is not defined! Terminating. "; exit 1; fi
+if [ -z ${REMOTE_SERVER_FILESYSTEM_CHARSET+x} ]; then echo "Variable 'REMOTE_SERVER_FILESYSTEM_CHARSET' is not defined! Terminating. "; exit 1; fi
+if [ -z ${LOCAL_MOUNTPOINT+x} ]; then echo "Variable 'LOCAL_MOUNTPOINT' is not defined! Terminating. "; exit 1; fi
+if [ -z ${LOCAL_BACKUP_USER_NAME+x} ]; then echo "Variable 'LOCAL_BACKUP_USER_NAME' is not defined! Terminating. "; exit 1; fi
+if [ -z ${LOCAL_BACKUP_USER_GROUP+x} ]; then echo "Variable 'LOCAL_BACKUP_USER_GROUP' is not defined! Terminating. "; exit 1; fi
+if [ -z ${BACKUP_TARGET_ROOT+x} ]; then echo "Variable 'BACKUP_TARGET_ROOT' is not defined! Terminating. "; exit 1; fi
+if [ -z ${INFLUX_USERNAME+x} ]; then echo "Variable 'INFLUX_USERNAME' is not defined! Terminating. "; exit 1; fi
+if [ -z ${INFLUX_PASSWORD+x} ]; then echo "Variable 'INFLUX_PASSWORD' is not defined! Terminating. "; exit 1; fi
+if [ -z ${INFLUX_HOSTNAME+x} ]; then echo "Variable 'INFLUX_HOSTNAME' is not defined! Terminating. "; exit 1; fi
+if [ -z ${INFLUX_DB+x} ]; then echo "Variable 'INFLUX_DB' is not defined! Terminating. "; exit 1; fi
+if [ -z ${INFLUX_MACHINE_LABEL+x} ]; then echo "Variable 'INFLUX_MACHINE_LABEL' is not defined! Terminating. "; exit 1; fi
+
+### End check of proper configuration
+
+CURRENT_DAY_NUM=`date '+%u'`
+BACKUP_TARGET_DIRECTORY="${BACKUP_TARGET_ROOT}/$CURRENT_DAY_NUM"
+
+
+if [ ! -d "${BACKUP_TARGET_DIRECTORY}" ]; then
+    mkdir -p "${BACKUP_TARGET_DIRECTORY}"
+fi
+
+if grep -qs "${LOCAL_MOUNTPOINT} " /proc/mounts; then
+    echo "Mount point already mounted. Trying unmount first."
+    umount "${LOCAL_MOUNTPOINT}"
+fi
+
+mount -t cifs -o "ro,iocharset=${REMOTE_SERVER_FILESYSTEM_CHARSET},username=${REMOTE_SERVER_USER_NAME},password=${REMOTE_SERVER_USER_PASSWORD},vers=3.0" "//${REMOTE_SERVER_IP}/${REMOTE_SERVER_DIRECTORY}" "${LOCAL_MOUNTPOINT}"
+
+rsync -o -g -a "${LOCAL_MOUNTPOINT}/" "${BACKUP_TARGET_DIRECTORY}"
+chown -R "${LOCAL_BACKUP_USER_NAME}:${LOCAL_BACKUP_USER_GROUP}" "${BACKUP_TARGET_DIRECTORY}"
+
+umount "${LOCAL_MOUNTPOINT}"
+
+curl \
+    --silent \
+    -o /dev/null \
+    --insecure \
+    -i \
+    --data-binary "synology_backups,server=${INFLUX_MACHINE_LABEL} finished=1" \
+    -XPOST "https://${INFLUX_USERNAME}:${INFLUX_PASSWORD}@${INFLUX_HOSTNAME}/write?db=${INFLUX_DB}"
+
---
+++ config.sh.tpl
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Uzivatelske jmeno pro pripojeni se k serveru na zalohovani
+REMOTE_SERVER_USER_NAME=
+# Heslo pro pripojeni se k serveru na zalohovani
+REMOTE_SERVER_USER_PASSWORD=
+# IP adresa serveru pro zazalohovani
+REMOTE_SERVER_IP=
+# Adresar na vzdalenem serveru, ktery se ma zazalohovat.
+REMOTE_SERVER_DIRECTORY=data
+# Charset filesystemu na vzdalenem serveru
+REMOTE_SERVER_FILESYSTEM_CHARSET=utf8
+
+# Lokalni adresar, na ktery se namountuje vzdaleny server.
+LOCAL_MOUNTPOINT=/volume1/data/mountserver
+
+# Nazev lokalniho uzivatele, v jehoz domovskem adresari se delaji zalohy.
+LOCAL_BACKUP_USER_NAME=server
+# Skupina, do ktere tento uzivatel patri.
+LOCAL_BACKUP_USER_GROUP=users
+
+# Lokalni adresar, kam se budou dělat zálohy. Zde vznikají adresáře 1, 2, 3...
+BACKUP_TARGET_ROOT="/volume1/data/zalohy"
+
+
+# konfigurace monitoringu - ponechat beze změn kromě INFLUX_MACHINE_LABEL
+INFLUX_USERNAME=stats
+INFLUX_PASSWORD=41---------4tY
+INFLUX_HOSTNAME=i-------t
+INFLUX_DB=tarzi
+INFLUX_MACHINE_LABEL=essa_cb
